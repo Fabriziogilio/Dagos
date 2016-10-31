dc99a20b3ae76cd21a61fddb632d5f00b6c4cf5e
Fabriziogilio <fabriziogilio@gmail.com>
2016-10-31 20:07:42 -0300

    Update medio&pase.php

--- src/medio&pase.php
+++ src/medio&pase.php
@@ -1,24 +1,11 @@
 <?php
-
-namespace Fabri;
-
-class MedioBoleto extends TarjetaMovi implements tarjeta{
-	function __construct(){
-		parent :: __construct();
-		$this->boletoColectivo = 4.25;
-		$this->trasbordo = 1.40;
-	}
-}
-
-
-class Pase extends TarjetaMovi{
-	function __construct(){
-		parent :: __construct();
-		$this->boletoColectivo = 0;
-		$this->boletoBici = 0;
-	}
-	function saldo(){}
-	function recargar(){}
-}
-
+namespace Poli\Tarjeta;
+class MedioBoleto extends Sube {
+  public function __construct() {
+    $this->descuento = 0.5;
+  }
+class PaseLibre extends Sube {
+  public function __construct() {
+    $this->descuento = 0;
+  }
 ?>

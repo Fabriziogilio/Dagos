09442659ab225a4d13f41788eb45ae516df32b17
Fabriziogilio <fabriziogilio@gmail.com>
2016-10-31 20:10:04 -0300

    Update transporte.php

--- src/transporte.php
+++ src/transporte.php
@@ -1,30 +1,30 @@
 <?php
-
-namespace Fabri;
-
-abstract class Transporte{
-	protected $tipo, $numero;
-	function returnTipo(){
-		return $this->tipo;
-	}
-	function numero(){
-		return $this->numero;
-	}
-}
-
-class Colectivo extends Transporte{
-	function __construct($numero){
-		$this->numero = $numero;
-		$this->tipo = "Colectivo";
-	}
-}
-
-
-class Bici extends Transporte{
-	function __construct($numero){
-		$this->numero = $numero;
-		$this->tipo = "Bicicleta";
-	}
-}
-
+namespace Poli\Tarjeta;
+abstract class Transporte {
+  protected $tipo;
+	
+  public function Tipo() {
+    return $this->tipo;
+  }
+	
+class Colectivo extends Transporte {
+  private $linea;
+  private $empresa;
+  public function __construct($linea, $empresa) {
+    $this->tipo = "colectivo";
+    $this->linea = $linea;
+    $this->empresa = $empresa;
+  }
+  public function Nombre() {
+    return $this->linea;
+  }
+class Bici extends Transporte {
+  private $numero;
+  public function __construct($numero) {
+    $this->tipo = "Bicicleta";
+    $this->numero = $numero;
+  }
+  public function Nombre() {
+    return $this->numero;
+  }
 ?>

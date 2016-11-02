74b367742cdef65862efa84cf78b6304751c3e0b
Fabriziogilio <fabriziogilio@gmail.com>
2016-11-02 20:12:17 -0300

    Update Test.php

--- test/Test.php
+++ test/Test.php
@@ -1,12 +1,16 @@
 <?php
 namespace Poli\Tarjeta;
+
 use PHPUnit\Framework\TestCase;
-class TarjetaTest extends TestCase {
+
+class Test extends TestCase {
+  
   public function testCargaSaldo() {
     $tarjeta = new TarjetaMovi;
     $tarjeta->recargar(272);
     $this->assertEquals($tarjeta->saldo(), 320, "Cuando cargo 272 deberia tener finalmente 320");
   }
+  
   public function testPagarViaje() {
     $tarjeta = new TarjetaMovi;
     $tarjeta->recargar(20);
@@ -14,11 +18,13 @@ class TarjetaTest extends TestCase {
     $tarjeta->pagar($colectivo, "2016/10/31 22:00");
     $this->assertEquals($tarjeta->saldo(), 20 - 2.64, "Se descontó el pasaje");
   }
+  
   public function testPagarViajeSinSaldo() {
     $tarjeta = new TarjetaMovi;
     $colectivo = new Colectivo("144 Negro", "Rosario Bus");
     $this->assertFalse($tarjeta->pagar($colectivo, "2016/10/31 22:00"), "No hay saldo suficiente");
   }
+  
   public function testTrasbordo() {
      $tarjeta = new TarjetaMovi;
      $tarjeta->recargar(20);
@@ -27,6 +33,7 @@ class TarjetaTest extends TestCase {
      $tarjeta->pagar($colectivo, "2016/10/31 22:45");
      $this->assertEquals($tarjeta->saldo(), 20 - 7.92, "Trasbordo");
   }
+  
   public function testNoTransbordo() {
         $tarjeta = new TarjetaMovi;
         $tarjeta->recargar(20);
@@ -35,4 +42,5 @@ class TarjetaTest extends TestCase {
         $tarjeta->pagar($colectivo, "2016/10/31 23:30");
         $this->assertEquals($tarjeta->saldo(), 20 - 10.56, "No hubo trasbordo");
   }
+  
 }

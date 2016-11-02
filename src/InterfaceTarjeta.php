541a866d080b01c01fe4d2cc3ac7186c8fe7836f
Fabriziogilio <fabriziogilio@gmail.com>
2016-11-02 20:27:20 -0300

    Create InterfaceTarjeta.php

---
+++ src/InterfaceTarjeta.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Poli\Tarjeta;
+
+interface InterfaceTarjeta{
+	public function pagar(Transporte $transporte, $fecha_y_hora);
+	public function recargar($monto);
+	public function saldo();
+	public function viajesRealizados();
+}

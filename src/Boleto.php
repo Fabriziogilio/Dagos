0106b91872a55af5ae89f4f57951cf76cf88b5f2
Fabriziogilio <fabriziogilio@gmail.com>
2016-11-03 18:56:20 -0300

    Update Boleto.php

--- src/Boleto.php
+++ src/Boleto.php
@@ -8,10 +8,21 @@ class Boleto {
   private $monto;
   private $transporte;
   private $tiempo;
+  
   public function __construct($tipo, $monto, $transporte, $tiempo) {
     $this->tipo = $tipo;
     $this->monto = $monto;
     $this->transporte = $transporte;
     $this->tiempo = $tiempo;
   }
+  
+  public function tipo(){
+    return $this-tipo;
+  }
+  
+  public function tiempo(){
+    return $this-tiempo;
+  }
+  
+  
 }

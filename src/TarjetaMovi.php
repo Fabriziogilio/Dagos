ba9b32419e170e4e9dd215e8c9cb04c428415fdd
Fabriziogilio <fabriziogilio@gmail.com>
2016-11-03 23:35:37 +0200

    Update TarjetaMovi.php

--- src/TarjetaMovi.php
+++ src/TarjetaMovi.php
@@ -28,7 +28,7 @@ class TarjetaMovi implements InterfaceTarjeta {
 	
   protected function pagarColectivo(Transporte $transporte, $fecha_y_hora) {
     $trasbordo = false;
-    if (count($this->viajes) > 0) {
+    if (count($this->viajesRealizados()) > 0) {
       if (strtotime($fecha_y_hora) - end($this->viajes)->tiempo() < 3600) {
         $trasbordo = true;
       }
@@ -73,4 +73,8 @@ class TarjetaMovi implements InterfaceTarjeta {
     return $this->saldo;
   }
 	
+  public function viajesRealizados() {
+    return $this->viajes;
+  }
+
 }

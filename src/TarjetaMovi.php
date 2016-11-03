4dd74db40f42d1a14032ebcb107de5a6b7702faa
Fabriziogilio <fabriziogilio@gmail.com>
2016-11-03 10:47:47 -0300

    Update TarjetaMovi.php

--- src/TarjetaMovi.php
+++ src/TarjetaMovi.php
@@ -7,20 +7,24 @@ class TarjetaMovi implements InterfaceTarjeta {
   private $viajes = [];
   private $saldo = 0;
   protected $descuento;
+  private $plus = 0;
+	
   public function __construct() {
     $this->descuento = 1.0;
   }
+	
   public function pagar(Transporte $transporte, $fecha_y_hora) {
     if ($transporte->tipo() == "colectivo") {
       return $this->pagarColectivo($transporte, $fecha_y_hora);
     }
     else if ($transporte->tipo() == "bici") {
       if ($this->saldo < 12) return false; 
-      $this->viajes[] = new Boleto($transporte->tipo(), 12, $transporte, strtotime($fecha_y_hora));
+      $this->viajes[] = new Boleto($transporte->tipo(), 12, $transporte->nombre(), strtotime($fecha_y_hora));
       $this->saldo -= 12;
     }
     return true;
   }
+	
   protected function pagarColectivo(Transporte $transporte, $fecha_y_hora) {
     $trasbordo = false;
     if (count($this->viajes) > 0) {
@@ -35,11 +39,20 @@ class TarjetaMovi implements InterfaceTarjeta {
     else {
       $monto = 2.64 * $this->descuento;
     }
-    if ($this->saldo < $monto) return false;
+    if ($this->saldo < $monto){
+	    if ($this->plus < 2){
+		    $this->plus += 1;
+	    }
+	    else
+	    {
+		    return false;
+	    }
+    }
     $this->viajes[] = new Boleto($transporte->tipo(), $monto, $transporte, strtotime($fecha_y_hora));
     $this->saldo -= $monto;
     return true;
   }
+	
   public function recargar($monto) {
     if ($monto == 272) {
       $this->saldo += 320;

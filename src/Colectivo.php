82fb60653f8ff1eb2b18dddeb835af77aee34f2e
Fabriziogilio <fabriziogilio@gmail.com>
2016-11-03 12:42:32 -0300

    Update Colectivo.php

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -5,11 +5,15 @@ namespace Poli\Tarjeta;
 class Colectivo extends Transporte {
   private $linea;
   private $empresa;
+  
   public function __construct($linea, $empresa) {
     $this->tipo = "Colectivo";
     $this->linea = $linea;
     $this->empresa = $empresa;
   }
+  
   public function nombre() {
     return $this->linea;
   }
+  
+}
